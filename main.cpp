c5e7dddbc9b6c8e4d19873c2be128df408f9cf87
matveybaykalov <70635348+matveybaykalov@users.noreply.github.com>
2020-10-06 11:18:25 +0300

    Update main.cpp
    Я убрал возведения в степень и вычисление факториала на каждой итерации. Теперь для расчёта новых значения используются значения с предыдущего шага.

--- main.cpp
+++ main.cpp
@@ -1,57 +1,63 @@
 #include <iostream>
 #include <cmath>
 
-
 int main() {
     using std::cin;
     using std::cout;
+    using std::endl;
 
     for (int i = 0; i < 256; ++i)
         cout<<i<<' '<<char(i)<<' ';
+    cout << endl;
 
 
     unsigned char a;
     cin>>a;
     if ((a>=48) && (a<=57))
-        cout<<"VVedina cifra";
+        cout<<"VVedina cifra" << endl;
     else if ((a>=65) && (a<=122))
-        cout<<"VVedina bukva";
-    else cout<<"Simvol neizvesten";
+        cout<<"VVedina bukva"<< endl;
+    else cout<<"Simvol neizvesten"<< endl;
 
     int x;
     cin>>x;
     switch (x%5){
         case 0:
-            cout<<-1;
+            cout<<-1<< endl;
             break;
         case 1:
-            cout<<x-1;
+            cout<<x-1<< endl;
             break;
         case 2:
-            cout<<5;
+            cout<<5<< endl;
             break;
         case 3:
-            cout<<-x;
+            cout<<-x<< endl;
             break;
         case 4:
-            cout<<pow(x, -2);
+            cout<<pow(x, -2)<< endl;
             break;
     }
 
-    int y, i;
-    float s;
+    int i, y;
+    double s, pogreshnost = 0.01;
     cin>>y;
     for (int l = 2; l<7; l++ ) {
-    s=y;
-    i=1;
-    while (std::abs(sin(y)-s)>=pow(10,-l)){
+        s=y;
+        i=1;
+        double y2=y;
+        int znak = -1;
         int f=1;
-        for (int j=1; j<=2*i+1; j++)
-            f =f*j;
-        s=s+pow(-1,i)*pow(y,2*i+1)/f;
-        i++;
-    }
-    cout<<pow(10,-l)<<' '<<i<<std::endl;
+        int y1=y;
+        while (std::abs(sin(y)-s)>=pogreshnost){
+            f= f*(2*i+1) * (2*i);
+            y2=y2*y1*y1;
+            s=s+znak*y2/f;
+            znak = -znak;
+            i++;
+        }
+        cout<<pogreshnost<<' '<<i<<endl;
+        pogreshnost/=10;
     }
     return 0;
 
